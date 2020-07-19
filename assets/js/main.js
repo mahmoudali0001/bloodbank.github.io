86d385f0a3fa1bab4a4218ca881aebdd733701d4
mahmoudali0001 <59316869+mahmoudali0001@users.noreply.github.com>
2020-07-19 19:13:15 +0200

    bloodbank

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        rtl: true,
+        nav:true,
+        dots:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+});
